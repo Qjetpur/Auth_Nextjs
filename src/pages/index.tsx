f1b7f086946a78fd333245edfe75f9755dfedae0
Qjetpur <qjetpur@gmail.com>
2024-08-15 15:11:41 +0530

    login sigup page update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
+// pages/index.js
+
 export default function Home() {
-  return (
-     <h1>Helloooo</h1>
-  );
-}
+    return null; // Optionally, return null or a simple message like "Redirecting..."
+  }
+  
+  export async function getServerSideProps(context: any) {
+    return {
+      redirect: {
+        destination: '/register',
+        permanent: false, // Set to true if you want this to be a permanent redirect (HTTP 308)
+      },
+    };
+  }
+  
